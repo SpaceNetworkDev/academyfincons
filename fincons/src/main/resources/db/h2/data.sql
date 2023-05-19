ff96072599dd5ebb725f82ea6a8c557735cb36af
biancapetrelli <80674937+biancapetrelli@users.noreply.github.com>
2023-05-19 10:06:43 +0200

    Add files via upload

---
+++ fincons/src/main/resources/db/h2/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO USERS VALUES (1, 'Mario', 'Rossi', 'mrossi@gmail.com', '336205992');
+INSERT INTO USERS VALUES (2, 'Francesca', 'Rossi', 'frossi@gmail.com', '3374022995');
+INSERT INTO USERS VALUES (3, 'Mario', 'Dileo', 'mdleo@gmail.com', '3385029448');
